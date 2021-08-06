70b3f14987b112851a73a7549cbef52ba855b8ca
arian kazemi <87134472+ariankazemii@users.noreply.github.com>
2021-08-06 12:02:44 +0430

    Add files via upload

---
+++ matrix.py
@@ -0,0 +1,12 @@
+class Matrix:
+    def __init__(self, matrix_string):
+        rows = matrix_string.split("\n")
+        self.matrix = [[int(col) for col in row.split(' ')] for row in rows]
+
+    def row(self, index):
+        return self.matrix[index - 1]
+
+    def column(self, index):
+        return [row[index - 1] for row in self.matrix]
+
+
